963a6f619bb5ee95d610208a8ac9d0f787ad416d
Krishna Govarthini T <govarthinikkt@gmail.com>
2024-08-18 14:06:28 +0530

    Create datatype.js

---
+++ Javascript/datatype.js
@@ -0,0 +1,31 @@
+// Practiced these tasks to understand data types in JavaScript
+
+// Exercise 1
+var age = 22;
+var favcolor = "pink";
+var isRaining = true;
+var result = isRaining ? "It's Raining" : "It's Not";
+console.log("My Name is Krishna");
+console.log("My Age is " + age);
+console.log("My fav color is " + favcolor);
+console.log(result);
+
+// Exercise 2
+var width = 5;
+var height = 10;
+var area_of_rec = width * height;
+console.log("Area is " + area_of_rec);
+
+// Exercise 3
+var isSunny = false;
+var result = isSunny ? "It is a sunny day!!" : "It is not sunny today";
+console.log(result);
+
+// Exercise 4
+var myAge = 22;
+var friendAge = 25;
+var totalAge = myAge + friendAge;
+var isOlder = myAge > friendAge ? "true" : "false";
+var favnum = 13;
+var doublednum = favnum * 2;
+console.log("My age is " + myAge + " Am I younger than my friend " + isOlder + " Doubled my favnum is " + doublednum);
